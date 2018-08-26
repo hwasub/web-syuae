bc81b3add368776f4019fe38b2578d00d5772ddc
Hwasub Lee <lee@hwasub.com>
2018-08-26 14:43:02 +0900

    Modify GA and minimizer, deployment strategy

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ set -e # halt script on error
 export GIT_SHA=`git rev-parse --short HEAD`
 
 # git clone from gh-pages
-git clone --depth=2 --branch=gh-pages https://${GH_TOKEN}@github.com/${GH_REF} ${HTML_FOLDER}
+# git clone --depth=2 --branch=gh-pages https://${GH_TOKEN}@github.com/${GH_REF} ${HTML_FOLDER}
 
 # build
 bundle exec jekyll build
@@ -20,6 +20,7 @@ git config --global user.email "lee@hwasub.com"
 git config --global user.name "Hwasub Lee"
 
 # git commit
+git init
 git add --all
-git commit -m "Deploy master:${GIT_SHA} on GitHub Pages"
-git push --force --quiet "https://${GH_TOKEN}@github.com/${GH_REF}" gh-pages:gh-pages
+git commit -m "Deploy master:${GIT_SHA} to GitHub Pages"
+git push --force --quiet "https://${GH_TOKEN}@github.com/${GH_REF}" master:gh-pages
